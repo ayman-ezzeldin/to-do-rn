3bf2405294e4ffd8f69be0e1e3d42949687688c7
Ayman ezz <aymanmae12@gmail.com>
2025-10-16 22:54:20 +0300

    start todo app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,31 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Color } from 'react-native/types_generated/Libraries/Animated/AnimatedExports'
+import { Ionicons } from '@expo/vector-icons'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      screenOptions={{}}
+    >
+      <Tabs.Screen name="index" options={{
+        title: 'Todos',
+        tabBarIcon:({color, size}) => (
+          <Ionicons name='flash-outline' color={color} size={size} />
+        )
+      }} />
+      
+      <Tabs.Screen name="settings" options={{
+        title: 'Settings',
+        tabBarIcon:({color, size}) => (
+          <Ionicons name='settings' color={color} size={size} />
+        )
+      }} />
+      
+
     </Tabs>
-  );
+  )
 }
+
+export default _layout
