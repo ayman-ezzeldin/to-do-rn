c10bf8aeef8900ea22b3e49be30378e95df716a9
Ayman ezz <aymanmae12@gmail.com>
2025-10-21 18:16:05 +0300

    add schema, convex functions work

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,16 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
+  const todos = useQuery(api.todos.getTodos)
+  console.log("todos: ",todos)
+
+  const addTodo = useMutation(api.todos.addTodo)
+
   return (
     <View
       style={{
@@ -14,6 +22,9 @@ export default function Index() {
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle dark mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=> addTodo({text:"Ayman ezz"})}>
+        <Text>Add a new todo</Text>
       </TouchableOpacity>
         
     </View>
